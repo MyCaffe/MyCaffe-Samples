5c4bb8f1a66a7ac6f95ca45519e42d921b6cdab2
ZoroDerVonCodier <daveb@signalpop.com>
2020-01-18 14:24:11 -0800

    Added python ImageClassification sample.

---
+++ ImageClassification/ImageClassification.python/ImageClassification.py
@@ -0,0 +1,59 @@
+import clr
+clr.AddReference('C:\Program Files\SignalPop\MyCaffe\MyCaffe.dll')
+from MyCaffe import *
+clr.AddReference('C:\Program Files\SignalPop\MyCaffe\MyCaffe.basecode.dll')
+from MyCaffe.basecode import *
+clr.AddReference('C:\Program Files\SignalPop\MyCaffe\MyCaffe.db.image.dll')
+from MyCaffe.db.image import *
+
+def onWriteLine(sender, e):
+    print(e.Message)
+
+log = Log('test')
+log.OnWriteLine += onWriteLine
+cancel = CancelEvent()
+factory = DatasetFactory()
+settings = SettingsCaffe()
+
+# Load all images into memory before training
+settings.ImageDbLoadMethod = IMAGEDB_LOAD_METHOD.LOAD_ALL
+# Use GPU ID = 0
+settings.GpuIds = '0'
+
+# Load the descriptors from their respective files
+# (installed by MyCaffe Test Application install)
+file = open("C:\\ProgramData\\MyCaffe\\test_data\\models\\mnist\\lenet_solver.prototxt", "r")
+strSolver = file.read()
+file = open("C:\\ProgramData\\MyCaffe\\test_data\\models\\mnist\\lenet_train_test.prototxt", "r")
+strModel = file.read()
+
+# Load the MNIST dataset descriptor.
+ds = factory.LoadDataset('MNIST')
+
+# Create a test project with the dataset and descriptors
+project = ProjectEx('Test')
+project.SetDataset(ds)
+project.ModelDescription = strModel
+project.SolverDescription = strSolver
+
+# Create the MyCaffeControl (with the 'float' base type)
+strCudaPath = "C:\\Program Files\\SignalPop\\MyCaffe\\CudaDnnDll.10.2.dll"
+mycaffe = MyCaffeControl[float](settings, log, cancel, None, None, None, None, strCudaPath)
+
+# Load the project, using the TRAIN phase
+mycaffe.Load(Phase.TRAIN, project)
+
+# Train the model for 5000 iterations
+# (which uses the internal solver and internal training net)
+nIterations = 5000
+mycaffe.Train(nIterations)
+
+# Test the model for 100 iterations
+# (which uses the internal solver and internal testing net)
+nIterations = 100
+dfAccuracy = mycaffe.Test(nIterations)
+
+# Report the testing accuracy.
+log.WriteLine('Accuracy = ' + str(dfAccuracy))
+
+mycaffe.Dispose()
