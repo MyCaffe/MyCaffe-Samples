dff2c1ef91ef51cc6f2cd6f612e3c5d880220c91
ZoroDerVonCodier <daveb@signalpop.com>
2020-08-06 15:04:53 -0700

    Updated for mycaffe v0.11.0.65.

--- ImageClassification/ImageClassification.python/ImageClassification.py
+++ ImageClassification/ImageClassification.python/ImageClassification.py
@@ -1,5 +1,5 @@
 # == Image Classification Sample ==
-# IMPORTANT: This sample requires the MyCaffe AI Platform version 0.10.2.124 or greater.
+# IMPORTANT: This sample requires the MyCaffe AI Platform version 0.11.0.65 or greater.
 # This sample requires:
 #   * Installing and running the MyCaffe AI Platform which is located at
 #     https://github.com/MyCaffe/MyCaffe/releases
@@ -52,7 +52,7 @@ project.ModelDescription = strModel
 project.SolverDescription = strSolver
 
 # Create the MyCaffeControl (with the 'float' base type)
-strCudaPath = "C:\\Program Files\\SignalPop\\MyCaffe\\CudaDnnDll.10.2.dll"
+strCudaPath = "C:\\Program Files\\SignalPop\\MyCaffe\\cuda_11.0\\CudaDnnDll.11.0.dll"
 mycaffe = MyCaffeControl[float](settings, log, cancel, None, None, None, None, strCudaPath)
 
 # Load the project, using the TRAIN phase
