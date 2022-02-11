55d92d9b85a5252de54dfb1d27a9e0d958995102
Dave Brown <daveb@signalpop.com>
2022-02-10 22:35:36 -0800

    Updated status

--- ImageClassification/ImageClassification/MainForm.Designer.cs
+++ ImageClassification/ImageClassification/MainForm.Designer.cs
@@ -32,6 +32,7 @@
             this.btnExportImages = new System.Windows.Forms.Button();
             this.btnSimplerClassification = new System.Windows.Forms.Button();
             this.btnSimplestClassification = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSimpleClassification
@@ -74,11 +75,22 @@
             this.btnSimplestClassification.UseVisualStyleBackColor = true;
             this.btnSimplestClassification.Click += new System.EventHandler(this.btnSimplestClassification_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "See Visual Studio Output window for debug output showing the status of each opera" +
+    "tion.";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 113);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSimplestClassification);
             this.Controls.Add(this.btnSimplerClassification);
             this.Controls.Add(this.btnExportImages);
@@ -87,6 +99,7 @@
             this.Text = "Image Classification";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +109,7 @@
         private System.Windows.Forms.Button btnExportImages;
         private System.Windows.Forms.Button btnSimplerClassification;
         private System.Windows.Forms.Button btnSimplestClassification;
+        private System.Windows.Forms.Label label1;
     }
 }
 
