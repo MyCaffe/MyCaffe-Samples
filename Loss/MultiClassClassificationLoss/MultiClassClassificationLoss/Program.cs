c7fce16ce50582e46b152fb9655c19c4bdef640b
Dave Brown <daveb@signalpop.com>
2022-06-27 08:32:08 -0700

    Changed namespace

--- Loss/MultiClassClassificationLoss/MultiClassClassificationLoss/Program.cs
+++ Loss/MultiClassClassificationLoss/MultiClassClassificationLoss/Program.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinaryClassificationLoss
+namespace MultiClassClassificationLoss
 {
     internal class Program
     {
