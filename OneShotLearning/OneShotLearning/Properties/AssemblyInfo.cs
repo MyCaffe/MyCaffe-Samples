6c17a917744a78f542894669393a4f663282045c
Dave Brown <daveb@signalpop.com>
2023-02-21 16:04:52 -0800

    Upgraded to MyCaffe 1.12.0.60

--- OneShotLearning/OneShotLearning/Properties/AssemblyInfo.cs
+++ OneShotLearning/OneShotLearning/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OneShotLearning")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020-2023 SignalPop LLC. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
