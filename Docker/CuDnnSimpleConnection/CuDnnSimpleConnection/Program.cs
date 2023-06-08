0a56fa15117e6590ac661d9f6de9f232ca0a6008
Dave Brown <daveb@signalpop.com>
2023-06-08 13:40:17 -0700

    Upgraded to MyCaffe 1.121.82

---
+++ Docker/CuDnnSimpleConnection/CuDnnSimpleConnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyCaffe.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuDnnSimpleConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CudaDnn<float> cuda = new CudaDnn<float>(0, (DEVINIT)3);
+
+            int nDevCount = cuda.GetDeviceCount();
+            for (int i = 0; i < nDevCount; i++)
+            {
+                Console.WriteLine("Device #" + i.ToString() + " = " + cuda.GetDeviceName(i));
+            }
+        }
+    }
+}
