cbd1ed7cb420aa54951d6bc8d9270cfc93dd26f7
Dave Brown <daveb@signalpop.com>
2023-09-18 08:56:26 -0700

    Upgraded to MyCaffe version 1.12.2.41

--- Loss/MeanErrorLoss/MeanErrorLoss/Program.cs
+++ Loss/MeanErrorLoss/MeanErrorLoss/Program.cs
@@ -16,9 +16,9 @@ namespace MeanErrorLoss
     internal class Program
     {
         /// <summary>
-        /// This sample requires version 1.12.0.60 or greater.
+        /// This sample requires version 1.12.2.41 or greater.
         /// </summary>
-        static string m_strExpectedMyCaffeVersion = "1.12.2.40";
+        static string m_strExpectedMyCaffeVersion = "1.12.2.41";
         /// <summary>
         /// Specifies the training dataset.
         /// </summary>
